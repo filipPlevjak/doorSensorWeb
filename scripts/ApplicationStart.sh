17171710093a5db312e5205bb79129558ce110c7
Filip Plevjak <54470003+filipPlevjak@users.noreply.github.com>
2022-02-06 09:49:32 +0100

    03
    03

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-##### Start http service
+##### Start Service app
 
-#sudo service httpd start
 cd /home/app/doorSensor/
-
+#####      Creating a service call doorSensor
 sudo forever-service install doorSensor -r app
-
-sudo service doorSebsir start
+##### start a service called doorSensor
+sudo service doorSensor start
